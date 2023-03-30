7a22e25a80970db0dfdec95ddc2a69103fce9363
Louisa Ongenyi <o.louisa@ymail.com>
2023-03-30 06:53:45 +0200

    Ajout premières routes

---
+++ kasa/src/APropos.js
@@ -0,0 +1,5 @@
+function APropos() {
+    return
+}
+
+export default APropos;
--- kasa/src/App.js
+++ kasa/src/App.js
@@ -1,25 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Home from './Home';
+import FicheLogement from './FicheLogement';
+import APropos from './APropos';
+import NotFound from './NotFound';
+import Header from './Header';
+import Footer from  './Footer';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+    <Header/>
+    <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/FicheLogement" element={<FicheLogement/>}/>
+        <Route path="/APropos" element={<APropos/>}/>
+        <Route path="/NotFound" element={<NotFound/>}/>
+    </Routes>  
+    <Footer/>
+    
+    </Router>
+
+ );
 }
 
 export default App;
