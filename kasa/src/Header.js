a41c993e4d6d512dab2e91e2e3b2004fe2152c7f
Louisa Ongenyi <o.louisa@ymail.com>
2023-03-30 12:08:34 +0200

    routage fonctionnel

--- kasa/src/Header.js
+++ kasa/src/Header.js
@@ -7,7 +7,7 @@ function Header() {
         <header>
             <nav>
                 <ul>
-                   <li><Link to="Home">Accueil</Link></li>
+                   <li><Link to="/">Accueil</Link></li>
 
                     <li><Link to="APropos">A propos</Link></li>
                 </ul>
--- kasa/src/NotFound.js
+++ kasa/src/NotFound.js
@@ -1,5 +1,15 @@
+import { Link } from "react-router-dom";
+
+
+
 function NotFound() {
-    return "Erreur 404"
+    return (
+        <div>
+        <h1>404</h1>
+        <h3>Oups! La page que vous demandez n'existe pas.</h3>
+        <Link to="/">Retourner vers la page d'accueil</Link>
+        </div>
+    )
 }
 
 export default NotFound;
