780e1c878a0cafce1499f9ea0166a73cfb01bd4f
Louisa Ongenyi <o.louisa@ymail.com>
2023-04-04 05:52:36 +0200

    Page d'accueil responsive corrections

--- kasa/src/Banner.js
+++ kasa/src/Banner.js
@@ -4,7 +4,7 @@ import Img from "./IMGbanner.png";
 
 function Banner() {
     return (
-    <div className="bannière">
+    <div className="banner">
         <div className="imgBannerdiv"><img className="imgBanner" src={Img} alt="Image de montagnes"/></div>
         <h3 className="TitleBanner">Chez vous, partout et ailleurs</h3>
     </div>
